1bcbc59fd5b458a89475393e5a6d3f2ed41e2bcc
David Lumley <david@davidlumley.com.au>
2013-11-19 13:26:07 +1000

    add really basic application

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,27 @@
 module SinatraApp
   class App < Sinatra::Base
 
+    before do
+      @current_product ||= Product.current
+    end
+
     get '/' do
       erb :index
+    end
+
+    post '/purchase/?' do
+      if @current_product.quantity >= params[:quantity].to_i
+
+        @current_product.quantity -= params[:quantity].to_i
+        @current_product.save
+
+        user     = User.first_or_create(:email => params[:email])
+
+        purchase          = user.purchases.new
+        purchase.product  = @current_product
+        purchase.quantity = params[:quantity].to_i
+        purchase.save
+      end
     end
 
   end
